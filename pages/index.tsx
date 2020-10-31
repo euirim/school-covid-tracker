7a545666581a10dbbff6428353e6e1f65a20c664
Euirim Choi <euirim@gmail.com>
2020-10-30 17:45:18 -0700

    Add data retrieval and indexing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,28 @@
-import { NextPage } from 'next';
+import Axios from 'axios';
+import { GetStaticProps, NextPage } from 'next';
+import CSVParse from 'csv-parse/lib/sync';
+import { useEffect, useState } from 'react';
+import { buildSearchIndex } from '@utils/search';
+import { Search } from 'js-search';
 
-const Home: NextPage<unknown> = () => {
+type Props = {
+  records: {
+    county: string;
+    school_or_school_district: string;
+    type: string;
+    student_cases_new: string;
+    student_cases_cumulative: string;
+    staff_cases_new: string;
+    staff_cases_cumulative: string;
+  }[];
+};
+
+const Home: NextPage<Props> = ({ records }) => {
+  const [searchIndex, setSearchIndex] = useState<Search | null>(null);
+  useEffect(() => {
+    setSearchIndex(buildSearchIndex(records));
+  }, []);
+
   return (
     <>
       <h1>Hello</h1>
@@ -9,3 +31,20 @@ const Home: NextPage<unknown> = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async (): Promise<any> => {
+  const res = await Axios.get(
+    'https://coronavirus.ohio.gov/static/dashboards/school_reporting.csv'
+  );
+  const csv = res.data;
+  const records = CSVParse(csv, {
+    columns: true,
+    skip_empty_lines: true,
+  });
+
+  return {
+    props: {
+      records,
+    },
+  };
+};
---
+++ utils/search.ts
@@ -0,0 +1,11 @@
+import { Search } from 'js-search';
+
+export const buildSearchIndex = (records: unknown[]) => {
+  const search = new Search('school_or_school_district');
+  search.addIndex('county');
+  search.addIndex('school_or_school_district');
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  search.addDocuments(records as Object[]);
+
+  return search;
+};
