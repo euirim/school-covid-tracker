b5e05146d9b194150c1f1b2c17f5adad415ebe7d
euirim <euirim@users.noreply.github.com>
2020-10-31 06:10:01 +0000

    Deploying to gh-pages from  @ d095a8615bf67bd840887112de0b7e4bd2be9bb9 🚀

---
+++ _next/static/7bTA2Qy03Y43wB_qzYfRU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-34b0a7c452a63fc29576.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-84b78e5f225a989595b8.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
