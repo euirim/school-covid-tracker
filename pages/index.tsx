2825125dfd654a08ef8e00bab5049a229cb68f20
Euirim Choi <euirim@gmail.com>
2020-11-01 20:02:26 -0800

    Fix title

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,13 +79,13 @@ const Home: NextPage<Props> = ({
   return (
     <>
       <Head>
-        <title>Ohio School COVID-19 Case Tracker</title>
+        <title>Ohio School/District COVID-19 Case Tracker</title>
       </Head>
       <div className="flex flex-col items-stretch h-screen lg:flex-row">
         <div>
           <div className="max-w-2xl px-5 py-8 mx-auto lg:h-screen lg:overflow-y-auto lg:px-12">
             <h1 className="mb-2 font-sans text-4xl font-bold leading-tight text-center text-gray-900 sm:leading-normal">
-              Ohio School COVID-19 Case Tracker
+              Ohio School/District COVID-19 Case Tracker
             </h1>
             <p className="text-center text-gray-600">
               Data last retrieved{' '}
@@ -111,7 +111,7 @@ const Home: NextPage<Props> = ({
             </div>
             <div style={{ marginBottom: '-1rem' }} className="lg:pb-4">
               <h2 className="mb-4 text-lg font-bold text-center text-gray-600 sm:text-xl">
-                Top 5 Schools With Most Cases
+                Top 5 Schools/Districts With Most Cases
               </h2>
               <SearchResults results={records.slice(0, 5)} onWhiteBg />
             </div>
